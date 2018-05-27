1c4d814568ee335d3c0fe2bf86ca700f9dc544a8
alfioanr <alfioanr@gmail.com>
2018-05-27 11:08:01 +0200

    First and only commit
    el primero del backend

---
+++ server/create-lb-table.js
@@ -0,0 +1,10 @@
+//create-lb-tables
+
+var server = require('./server');
+var ds = server.dataSources.db;
+var lbTables = ['User','AccessToken', 'ACL', 'RoleMapping', 'Role', 'Challenge'];
+ds.automigrate(lbTables, function(er) {
+ if (er) throw er;
+ console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+ ds.disconnect();
+});
